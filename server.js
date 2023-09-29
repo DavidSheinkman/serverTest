c7a811fa97ce0575473a8ae7b59e7e2619c9fe66
DavidSheinkman <112998060+DavidSheinkman@users.noreply.github.com>
2023-09-30 01:12:31 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,32 +1,12 @@
-const { MongoClient } = require('mongodb');
+const { MongoClient } = require("mongodb");
+
 const express = require("express");
 const cors = require("cors");
 const app = express();
 app.use(cors());
 
 const PORT = process.env.PORT || 5000;
-const mongoURI = 'mongodb+srv://david:Poilkjmnb!@cluster0.yshjcqa.mongodb.net/zoo?retryWrites=true&w=majority';
 
-// Create a MongoDB client instance and connect to the database
-const client = new MongoClient(mongoURI);
-
-
-async function startServer() {
-  try {
-    // Connect to the MongoDB database
-    await client.connect();
-    console.log("Connected to MongoDB");
-
-    // Start the Express server after the database connection is established
-    app.listen(PORT, () => {
-      console.log(`Server started on port ${PORT}`);
-    });
-  } catch (error) {
-    console.error("Error connecting to MongoDB:", error);
-  }
-}
-
-startServer();
 
 app.get("/api", (req, res) => {
   res.json(Pictures);
@@ -36,20 +16,30 @@ app.get("/api/:id", async (req, res) => {
   const meetupId = req.params.id;
 
   try {
+    const client = await MongoClient.connect(
+      "mongodb+srv://david:Poilkjmnb!@cluster0.yshjcqa.mongodb.net/zoo?retryWrites=true&w=majority"
+    );
     const db = client.db();
-    const animals = db.collection('animals');
-    
-    const query = { id: meetupId };
+
+    const animals = db.collection("animals");
+    console.log(animals);
+
+    const query = { ["id"]: meetupId };
     const result = await animals.findOne(query);
     console.log(result);
+    client.close();
 
     if (result) {
       res.json(result);
     } else {
-      res.status(404).json({ message: "Meetup not found" });
+      res.status(404).json({ message: "Meetup not found" , error: error.message});
     }
   } catch (error) {
     console.error("Error:", error);
-    res.status(500).json({ message: "Internal Server Error", error: error.message });
+    res.status(500).json({ message: "Internal Server Error" });
   }
+});
+
+app.listen(PORT, () => {
+  console.log(`server started on port ${PORT}`);
 });
