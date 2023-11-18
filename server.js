a63f7aeac3107bccf2533ffb00e2d7e0c80a0a66
DavidSheinkman <112998060+DavidSheinkman@users.noreply.github.com>
2023-11-18 17:30:51 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -6,19 +6,34 @@ const app = express();
 app.use(cors());
 
 const PORT = process.env.PORT || 5000;
+const mongoDBConnectionString =
+  "mongodb+srv://david:Poilkjmnb!@cluster0.yshjcqa.mongodb.net/zoo?retryWrites=true&w=majority";
 
+//get all animals
+app.post("/api", async (req, res) => {
+  try {
+    const client = await MongoClient.connect(mongoDBConnectionString);
+    const db = client.db();
 
-app.get("/api", (req, res) => {
-  res.json(Pictures);
+    const animals = db.collection("animals");
+    const result = await animals.insertOne(req.body);
+    client.close();
+
+    res.json(result.ops[0]);
+  } catch (error) {
+    console.error("Error:", error);
+    res
+      .status(500)
+      .json({ message: "Internal Server Error", error: error.message });
+  }
 });
 
+//get animal by id
 app.get("/api/:id", async (req, res) => {
   const meetupId = req.params.id;
 
   try {
-    const client = await MongoClient.connect(
-      "mongodb+srv://david:Poilkjmnb!@cluster0.yshjcqa.mongodb.net/zoo?retryWrites=true&w=majority"
-    );
+    const client = await MongoClient.connect(mongoDBConnectionString);
     const db = client.db();
 
     const animals = db.collection("animals");
@@ -32,11 +47,70 @@ app.get("/api/:id", async (req, res) => {
     if (result) {
       res.json(result);
     } else {
-      res.status(404).json({ message: "Meetup not found" });
+      res.status(404).json({ message: "Animal not found" });
     }
   } catch (error) {
     console.error("Error:", error);
-    res.status(500).json({ message: "Internal Server Error", error: error.message });
+    res
+      .status(500)
+      .json({ message: "Internal Server Error", error: error.message });
+  }
+});
+
+//update animal by id
+app.put("/api/:id", async (req, res) => {
+  const meetupId = req.params.id;
+
+  try {
+    const client = await MongoClient.connect(mongoDBConnectionString);
+    const db = client.db();
+
+    const animals = db.collection("animals");
+    const query = { ["id"]: meetupId };
+    const updateDoc = {
+      $set: req.body, // Assuming the request body contains the updated data
+    };
+
+    const result = await animals.updateOne(query, updateDoc);
+    client.close();
+
+    if (result.matchedCount > 0) {
+      res.json({ message: "Animal updated successfully" });
+    } else {
+      res.status(404).json({ message: "Animal not found" });
+    }
+  } catch (error) {
+    console.error("Error:", error);
+    res
+      .status(500)
+      .json({ message: "Internal Server Error", error: error.message });
+  }
+});
+
+//delete animal by id
+app.delete("/api/:id", async (req, res) => {
+  const meetupId = req.params.id;
+
+  try {
+    const client = await MongoClient.connect(mongoDBConnectionString);
+    const db = client.db();
+
+    const animals = db.collection("animals");
+    const query = { ["id"]: meetupId };
+
+    const result = await animals.deleteOne(query);
+    client.close();
+
+    if (result.deletedCount > 0) {
+      res.json({ message: "Animal deleted successfully" });
+    } else {
+      res.status(404).json({ message: "Animal not found" });
+    }
+  } catch (error) {
+    console.error("Error:", error);
+    res
+      .status(500)
+      .json({ message: "Internal Server Error", error: error.message });
   }
 });
 
